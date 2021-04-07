d4419be6fc5feb0d69bf1d98f1742dd1f1f4ba41
junchen14 <jun.chen@kaust.edu.sa>
2021-04-07 09:07:57 +0300

    upload code

---
+++ evaluation/cider/cider.py
@@ -0,0 +1,34 @@
+
+from .cider_scorer import CiderScorer
+
+class Cider:
+    """
+    Main Class to compute the CIDEr metric
+
+    """
+    def __init__(self, gts=None, n=4, sigma=6.0):
+        # set cider to sum over 1 to 4-grams
+        self._n = n
+        # set the standard deviation parameter for gaussian penalty
+        self._sigma = sigma
+        self.doc_frequency = None
+        self.ref_len = None
+        if gts is not None:
+            tmp_cider = CiderScorer(gts, n=self._n, sigma=self._sigma)
+            self.doc_frequency = tmp_cider.doc_frequency
+            self.ref_len = tmp_cider.ref_len
+
+    def compute_score(self, gts, res):
+        """
+        Main function to compute CIDEr score
+        :param  gts (dict) : dictionary with key <image> and value <tokenized hypothesis / candidate sentence>
+                res (dict)  : dictionary with key <image> and value <tokenized reference sentence>
+        :return: cider (float) : computed CIDEr score for the corpus
+        """
+        assert(gts.keys() == res.keys())
+        cider_scorer = CiderScorer(gts, test=res, n=self._n, sigma=self._sigma, doc_frequency=self.doc_frequency,
+                                   ref_len=self.ref_len)
+        return cider_scorer.compute_score()
+
+    def __str__(self):
+        return 'CIDEr'
