8714285a447de08dc02e7d5a8074b151595a5db2
junchen14 <jun.chen@kaust.edu.sa>
2021-04-19 14:48:36 +0300

    clean

--- models/transformer/__init__.py
+++ models/transformer/__init__.py
@@ -1,9 +1,6 @@
 from .transformer import Transformer
 from .encoders import *
-from .encoders_aoa_baseline import MemoryAugmentedEncoder_baseline
 from .attention import *
-from .transformer_m2 import Transformer_m2
-from .m2_attention import *
 
 from .transformer_visualGPT import Transformer_visualgpt
 
